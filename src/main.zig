0dffce808a4df1f76b0b708be786d5ede59ee5ff
Lailouezzz <alanlebouder@gmail.com>
2024-01-14 16:41:28 +0100

    Update: main.

--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,29 @@ const glfw = @import("glfw.zig");
 
 const Engine = @import("engine/Engine.zig");
 
+fn keycb(pointer: ?*anyopaque, key: glfw.Key, scancode: i32, action: glfw.Action, mods: glfw.Mods) void {
+	const engine = @as(?*Engine, @ptrCast(@alignCast(pointer.?))).?;
+	_ = key;
+	_ = scancode;
+	_ = action;
+	_ = mods;
+	std.log.info("Key press. {}", .{engine.window.width});
+}
+
 pub fn main() !void {
+	// var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+	// defer arena.deinit();
+	// const allocator = arena.allocator();
+	const allocator = std.heap.c_allocator;
 	try glfw.initError();
 	defer glfw.deinit();
-	var engine = try Engine.init("test", .{
+	var engine: Engine = undefined;
+	engine = try Engine.init("test", .{
 		.height = 500,
 		.width = 500,
-	});
+		.pointer = &engine,
+		.keycb = keycb,
+	}, allocator);
 	defer engine.deinit();
 	try engine.run();
 }
